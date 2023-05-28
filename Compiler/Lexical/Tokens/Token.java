737dc48b80bb726b8bd03edd24e5e375c79f030a
Rodrigo Luiz Panta Junior <rodrigo.panta@dtidigital.com.br>
2023-05-27 22:51:36 -0300

    change tag to private

--- Compiler/Lexical/Tokens/Token.java
+++ Compiler/Lexical/Tokens/Token.java
@@ -2,7 +2,7 @@ package Compiler.Lexical.Tokens;
 
 public class Token {
 
-    public final int tag; //constante que representa o token
+    private final int tag; //constante que representa o token
 
     public Token(int t) {
         tag = t;
